d8399abfdcf62d5d987be1f36692d6adc1cbe2e1
Luis Zimermann <luisaslan@gmail.com>
2022-05-19 11:25:24 -0300

    Adicionando SQLIte

--- cardapio.sql
+++ cardapio.sql
@@ -24,19 +24,19 @@ INSERT INTO category (name) VALUES ("Combinados");
 INSERT INTO category (name) VALUES ("Sobremesas");
 INSERT INTO category (name) VALUES ("Bebidas");
 
-INSERT INTO product (name, amount, category_id,description, thumb_url) 
+INSERT INTO product (name, amount, category_id, description, thumb_url) 
 VALUES ("X Salada", 1800, 1, "200g de carne, mussarela, alface, tomate, picles, cebola roxa e maionese caseira", "https://www.seekpng.com/png/detail/408-4083760_png-hamburguer-po-de-hamburguer-congelado.png");
 
-INSERT INTO product (name, amount, category_id,description, thumb_url) 
+INSERT INTO product (name, amount, category_id, description, thumb_url) 
 VALUES ("X Bacon", 2200, 1, "300g de carne, bacon, cheddar, alface, tomate, cebola roxa e maionese caseira", "https://www.kindpng.com/picc/m/427-4271828_transparent-hamburguer-png-hamburger-png-png-download.png");
 
-INSERT INTO product (name, amount, category_id,description, thumb_url) 
+INSERT INTO product (name, amount, category_id, description, thumb_url) 
 VALUES ("Batata frita", 900, 2, "500g de batata.", "https://static.clubedaanamariabraga.com.br/wp-content/uploads/2019/02/batata-frita-sequinha-crocante.jpeg");
 
-INSERT INTO product (name, amount, category_id,description, thumb_url) 
+INSERT INTO product (name, amount, category_id, description, thumb_url) 
 VALUES ("Combinado", 3000, 3, "X salada + 250g de Batata + Refri", "https://www.imagensempng.com.br/wp-content/uploads/2021/02/Hamburguer-Coca-Cola-Batata-Frita-Png.png");
 
-INSERT INTO product (name, amount, category_id,description, thumb_url) 
+INSERT INTO product (name, amount, category_id, description, thumb_url) 
 VALUES ("Mousse", 600, 4, "Mousse de chocolate", "https://mrbey.com.br/wp-content/uploads/2021/02/Mousse-Lacta-Blue-500x338-01.jpg");
 
 INSERT INTO product (name, amount, category_id, description, thumb_url) 
